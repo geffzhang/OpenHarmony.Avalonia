a4d57d9f92fd586198158e8e2ee19d6b3d4cfe15
CeSun <ce.sun@outlook.com>
2024-11-04 00:32:30 +0800

    icu和openssl已经完全支持

--- OHOS_Project/AvaloniaNative/src/main/cpp/napi_init.cpp
+++ OHOS_Project/AvaloniaNative/src/main/cpp/napi_init.cpp
@@ -2,14 +2,10 @@
 #include <dlfcn.h>
 #include <stdlib.h>
 #include <stdint.h>
-#define _TEST_ICU_ false
-
-void InitICU();
 
 extern "C" __attribute__((constructor)) void RegisterAvaloniaNativeModule(void)
 {
-    InitICU();
-    
+    setenv("ICU_DATA", "/system/usr/ohos_icu", 0);
     auto handle = dlopen("libavalonia.so", RTLD_NOW);
     assert(handle != nullptr);
     
@@ -17,26 +13,4 @@ extern "C" __attribute__((constructor)) void RegisterAvaloniaNativeModule(void)
     assert(pRegisterEntryModule != nullptr);
     
     pRegisterEntryModule();
-}
-
-#define U_MAX_VERSION_LENGTH 4
-typedef uint8_t UVersionInfo[U_MAX_VERSION_LENGTH];
-
-void InitICU()
-{
-    setenv("ICU_DATA", "/system/usr/ohos_icu", 0);
-    // test code
-    if (_TEST_ICU_){
-        auto handle = dlopen("libicui18n.so.76", RTLD_LAZY);
-        assert(handle != nullptr);
-        
-        auto pUlocdata_getCLDRVersion = (void(*)(UVersionInfo, int32_t*))dlsym(handle, "ulocdata_getCLDRVersion_76");
-        assert(pUlocdata_getCLDRVersion != nullptr);
-        
-        UVersionInfo version;
-        int32_t errorCode;
-        
-        pUlocdata_getCLDRVersion(version, &errorCode);
-        assert(errorCode == 0);
-    }
 }
