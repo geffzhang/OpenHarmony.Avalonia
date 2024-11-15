1b9f8c0a4b4128aa51a5a63d1ef796e226d8d9b3
CeSun <ce.sun@outlook.com>
2024-11-15 19:57:05 +0800

    更新版本

---
+++ Src/Avalonia.OpenHarmony/OpenHarmonyInputMethod.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia.Input.TextInput;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avalonia.OpenHarmony;
+
+public class OpenHarmonyInputMethod : ITextInputMethodImpl
+{
+    public void Reset()
+    {
+    }
+
+    public void SetClient(TextInputMethodClient? client)
+    {
+    }
+
+    public void SetCursorRect(Rect rect)
+    {
+    }
+
+    public void SetOptions(TextInputOptions options)
+    {
+    }
+}
